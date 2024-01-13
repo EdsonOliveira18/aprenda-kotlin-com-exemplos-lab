cd273e8cb9f89a7beeb3b98c7916cce533d03fef
EdsonOliveira18 <eedsoncarlos359@gmail.com>
2024-01-13 09:39:58 -0300

    conclusão do desafio de projeto

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,45 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+/// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(
+    val nome: String,
+    var nivel: Nivel,
+    var conteudos: MutableList<ConteudoEducacional>,
+    var inscritos: MutableList<Usuario> = mutableListOf()
+) {
 
-    val inscritos = mutableListOf<Usuario>()
-    
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+    }
+
+    fun exibirInscritos() {
+        println("Inscritos na formação '$nome' ($nivel):")
+        inscritos.forEachIndexed { index, usuario ->
+            println("${index + 1}. ${usuario.nome}")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val conteudo1 = ConteudoEducacional("Introdução a Kotlin")
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin")
+
+    val formacaoKotlin = Formacao(
+        "Formação Kotlin",
+        Nivel.INTERMEDIARIO,
+        mutableListOf(conteudo1, conteudo2)
+    )
+
+    val usuario1 = Usuario("João da Silva")
+    val usuario2 = Usuario("Maria Souza")
+
+    formacaoKotlin.matricular(usuario1)
+    formacaoKotlin.matricular(usuario2)
+
+    formacaoKotlin.exibirInscritos()
 }
